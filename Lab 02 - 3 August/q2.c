4d7b33be82baebbc544e586d9b5152a510c6d03b
Ajay Khatri <amajaying@gmail.com>
2023-08-03 10:23:35 +0530

    code

--- Lab 02 - 3 August/q2.c
+++ Lab 02 - 3 August/q2.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void gcd(FILE *ofp, FILE * fp){
-    int a, b;
-    fscanf(fp, "%d %d", &a, &b);
-    if(b==0){
-        fprintf(ofp, "%d\n", a);
-        return ;
+void gcd(int a, int b, FILE *ofp){
+    
+    if(a>b)
+        a=a-b;
+    else
+        b=b-a;
+    
+    if(a==0){
+        fprintf(ofp, "%d\n", b);
+        return;
     }
-    gcd(ofp, fp);
-    fprintf(ofp, "%d\n", b);
+    else if(b==0){
+        fprintf(ofp, "%d\n", a);
+        return;
+    }
+    else
+        gcd(a, b, ofp);
 }
+
 int main(int args, char* argv[]){
     char *inpfile = argv[1];
     char *outfile = argv[2];
     FILE *fp, *ofp;
     fp=fopen(inpfile,"r");
     ofp=fopen(outfile,"w");
-
+    while(!feof(fp)){
+        int a, b;
+        fscanf(fp, "%d %d", &a, &b);
+        fprintf(ofp, "The GCD of %d & %d is: ", a,b);
+        gcd(a, b, ofp);
+    }
 }
