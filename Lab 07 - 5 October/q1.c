6119233a1a53a4c3da9d46e8c2b886f4360202e9
Ajay Khatri <amajaying@gmail.com>
2023-10-05 10:53:58 +0530

    codes

--- Lab 07 - 5 October/q1.c
+++ Lab 07 - 5 October/q1.c
@@ -1,65 +1,88 @@
-// Given an undirected weighted connected graph G(V, E) and starring vertex ‘s’. Maintain a Min-Priority Queue ‘Q’ from the vertex set V and apply Prim’s algorithm to  Find the minimum spanning tree T(V, E’). Display the cost adjacency matrix of ‘T’.  Display total cost of the minimum spanning tree T. Note# Nodes will be numbered consecutively from 1 to n (user input), and edges will have varying weight. The graph G can be read from an input file “inUnAdjMat.dat” that contains cost adjacency matrix. The expected output could be displayed as the cost adjacency matrix of the minimum spanning tree and total cost of the tree.
+// Aim of the program: Given an undirected weighted connected graph G(V, E) and
+// starring vertex ‘s’. Maintain a Min-Priority Queue ‘Q’ from the vertex set V and
+// apply Prim’s algorithm to
+//  Find the minimum spanning tree T(V, E’). Display the cost adjacency matrix
+// of ‘T’.
+//  Display total cost of the minimum spanning tree T.
+// Note# Nodes will be numbered consecutively from 1 to n (user input), and edges will
+// have varying weight. The graph G can be read from an input file “inUnAdjMat.dat”
+// that contains cost adjacency matrix. The expected output could be displayed as the
+// cost adjacency matrix of the minimum spanning tree and total cost of the tree.
+
+// Content of the input file “inUnAdjMat.dat”
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
 
-int main()
-{
-    FILE *fp = fopen("inUnAdjMat.dat", "r");
-    if (fp == NULL)
-    {
-        printf("Error opening file\n");
-        exit(0);
-    }
+int main(){
     int n;
     printf("Enter the number of vertices: ");
     scanf("%d", &n);
-    int adjMat[n][n];
-    printf("Enter the starting vertex: ");
     int s;
+    printf("Enter the starting vertex: \n");
     scanf("%d", &s);
+    int adj[n][n];
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            fscanf(fp, "%d", &adjMat[i][j]);
+    FILE *fp;
+    fp = fopen("inUnAdjMat.dat", "r");
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            fscanf(fp, "%d ", &adj[i][j]);
         }
     }
+    fclose(fp);
 
+    // maintain a min-priority queue Q
+    int Q[n];
+    for(int i=0; i<n; i++){
+        Q[i] = INT_MAX;
+    }
+    Q[s-1] = 0;
+
+    // maintain a parent array
+    int parent[n];
+    for(int i=0; i<n; i++){
+        parent[i] = -1;
+    }
+
+    // maintain a visited array
     int visited[n];
-    for (int i = 0; i < n; i++)
-    {
+    for(int i=0; i<n; i++){
         visited[i] = 0;
     }
-    visited[s] = 1;
 
-    int totalWeight = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if (visited[i] == 1)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (visited[j] == 0)
-                {
-                    if (adjMat[i][j] != 0)
-                    {
-                        totalWeight += adjMat[i][j];
-                        visited[j] = 1;
-                    }
-                }
+    // apply Prim's algorithm
+    int u;
+
+    while(1){
+        int min = INT_MAX;
+        for(int i=0; i<n; i++){
+            if(Q[i] < min && visited[i] == 0){
+                min = Q[i];
+                u = i;
+            }
+        }
+        if(min == INT_MAX){
+            break;
+        }
+        visited[u] = 1;
+        for(int v=0; v<n; v++){
+            if(adj[u][v] != 0 && visited[v] == 0 && adj[u][v] < Q[v]){
+                Q[v] = adj[u][v];
+                parent[v] = u;
             }
         }
     }
 
-    // print the visited elements as a matrix and print 0 if not visited
-    
+    // display the cost adjacency matrix of T
+
+    printf("The cost adjacency matrix of T is: \n");
+
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            if(visited[i] == 1 && visited[j] == 1){
-                printf("%d ", adjMat[i][j]);
+            if(parent[i] == j || parent[j] == i){
+                printf("%d ", adj[i][j]);
             }
             else{
                 printf("0 ");
@@ -68,7 +91,18 @@ int main()
         printf("\n");
     }
 
-    
+    // display the total cost of T
 
-    printf("Total weight of the spanning Tree: %d\n", totalWeight);
+    int totalCost = 0;
+    for(int i=0; i<n; i++){
+        if(parent[i] != -1){
+            totalCost += adj[i][parent[i]];
+        }
+    }
+    printf("The total cost of T is: %d\n", totalCost);
+
+    return 0;
+
+    
 }
+
